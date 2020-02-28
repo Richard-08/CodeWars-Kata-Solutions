c41b6ad3b8b268919571ee657b0232f70ccffc54
Bulat/A <bulatahtyamov1995@mail.ru>
2020-02-28 16:02:34 +0300

    array prototype

---
+++ 6 kyu/Simple Web Framework #1.js
@@ -0,0 +1,45 @@
+/* In this Kata, you have to design a simple routing class for a web framework.
+
+The router should accept bindings for a given url, http method and an action.
+
+Then, when a request with a bound url and method comes in, it should return the result of the action.
+
+Example usage:
+
+var router = new Router;
+router.bind('/hello', 'GET', function(){ return 'hello world'; });
+
+router.runRequest('/hello', 'GET') // returns 'hello world';
+
+When asked for a route that doesn't exist, router should return:
+
+'Error 404: Not Found'
+
+The router should also handle modifying existing routes. See the example tests for more details.
+ */
+
+
+/* /////////////////////// Solution //////////////////////// */
+
+
+class Router {
+    constructor() {
+        this.routes = new Map;
+    }
+
+    bind(url, method, action) {
+        this.routes.set(`${url}:${method}`, action);
+    }
+
+    runRequest(url, method) {
+        if (!this.routes.has(`${url}:${method}`)) {
+            return 'Error 404: Not Found';
+        } else {
+            return this.routes.get(`${url}:${method}`)();
+        }
+    }
+}
+
+let router = new Router;
+router.bind('/hello', 'GET', function(){ return 'hello world'; });
+console.log(router.runRequest('/hello', 'GET')) // returns 'hello world';
